d161e6d649911ce708a5b9b2c5255648445014ee
deshpandeprasad <deshpande.prasa@northeastern.edu>
2023-11-23 04:28:06 -0500

    Deloped: Set up Sign Up Alerts

--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -1,12 +1,50 @@
 import { Box, Button, TextField } from "@material-ui/core";
 import React, { useState } from "react";
+import { CryptoState } from "../../CryptoContext";
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function Signup(handleClose) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmpassword, setConfirmpassword] = useState("");
 
-  const handleSubmit = () => {};
+  const { setAlert } = CryptoState();
+
+  const handleSubmit = async () => {
+    if (password !== confirmpassword) {
+      setAlert({
+        open: true,
+        message: "Passwords do not match",
+        type: "error",
+      });
+      return;
+    }
+
+    try {
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      console.log(result);
+
+      setAlert({
+        open: "true",
+        message: `Sign Up Successful, Welcome ${result.user.email}`,
+        type: "success",
+      });
+      handleClose();
+    } catch (error) {
+      setAlert({
+        open: "true",
+        message: error.message,
+        type: "error",
+      });
+      return;
+    }
+  };
 
   return (
     <Box
