1d66c3db87ea833c460422ef4e385c3bf79c68ca
deshpandeprasad <deshpande.prasa@northeastern.edu>
2023-11-23 02:54:14 -0500

    Developed: basic configuration of firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCKj_pvfRx5heghCycGzwM5-P2StwBOsqM",
+  authDomain: "cryptohunter12.firebaseapp.com",
+  projectId: "cryptohunter12",
+  storageBucket: "cryptohunter12.appspot.com",
+  messagingSenderId: "604147237606",
+  appId: "1:604147237606:web:075e81b0d4dfd937ef7127",
+};
+
+export default firebaseConfig;
