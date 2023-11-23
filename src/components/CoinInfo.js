d3d087066ca74ba777e96d3ff17eed19ac9ac779
deshpandeprasad <deshpande.prasa@northeastern.edu>
2023-11-22 23:30:10 -0500

    Developed: Historical Chart added

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,5 +1,114 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import { CryptoState } from "../CryptoContext";
+import axios from "axios";
+import { HistoricalChart } from "../config/api";
+import {
+  CircularProgress,
+  ThemeProvider,
+  createTheme,
+  makeStyles,
+} from "@material-ui/core";
+import { Line } from "react-chartjs-2";
 
-export const CoinInfo = () => {
-  return <div></div>;
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: "75%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 25,
+    padding: 40,
+    [theme.breakpoints.down("md")]: {
+      width: "100%",
+      marginTop: 0,
+      padding: 20,
+      paddingTop: 0,
+    },
+  },
+}));
+
+export const CoinInfo = ({ coin }) => {
+  const [historicalData, setHistoricalData] = useState();
+  const [days, setDays] = useState(1);
+
+  const { currency } = CryptoState();
+
+  const classes = useStyles();
+
+  const fetchHistoricalData = async () => {
+    try {
+      const { data } = await axios.get(
+        HistoricalChart(coin.id, days, currency)
+      );
+      setHistoricalData(data.prices);
+    } catch (error) {
+      console.error("Error fetching historical data:", error);
+      // Handle the error appropriately
+    }
+  };
+
+  useEffect(() => {
+    fetchHistoricalData();
+  }, [currency, days]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={classes.container}>
+        {/* chart */}
+
+        {!historicalData ? (
+          <CircularProgress
+            style={{ color: "gold" }}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicalData.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12} : ${date.getMinutes()} PM`
+                      : `${date.getHours()} : ${date.getMinutes()} AM`;
+
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+
+                datasets: [
+                  {
+                    data: historicalData.map((coin) => coin[1]),
+                    label: `Price ( Past ${days} ) in ${currency}`,
+                    borderColor: "EEBC1D",
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  point: {
+                    radius: 1,
+                  },
+                },
+              }}
+            />
+          </>
+        )}
+
+        {/* buttons */}
+      </div>
+    </ThemeProvider>
+  );
 };
